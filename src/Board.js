1203d18612cb1fd5c8a076d68c7d5dbbf91aa772
Hack Reactor Students <gjtrowbridge@gmail.com>
2014-06-11 15:26:27 -0700

    Implemented minor diagonal collision checking.

--- src/Board.js
+++ src/Board.js
@@ -134,14 +134,12 @@
       var n = rows.length;
 
       var rowNum = 0;
-      var colNum = majorDiagonalColumnIndexAtFirstRow;
       var curSum = 0;
       for(var i = majorDiagonalColumnIndexAtFirstRow; i < n; i++){
-        if( this._isInBounds(rowNum, colNum) ){
-          curSum += rows[rowNum][colNum];
+        if( this._isInBounds(rowNum, i) ){
+          curSum += rows[rowNum][i];
         }
         rowNum++;
-        colNum++;
       }
       return curSum > 1;
     },
@@ -164,12 +162,30 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var rows = this.rows();
+      var n = this.get('n');
+
+      var rowNum = 0;
+      var curSum = 0;
+      for (var i=minorDiagonalColumnIndexAtFirstRow; i>=0; i--) {
+        if (this._isInBounds(rowNum, i)) {
+          curSum += rows[rowNum][i];
+        }
+        rowNum++;
+      }
+      return curSum > 1;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var n = this.get('n');
+      for (var i=2*n -1; i>0; i--) {
+        if (this.hasMinorDiagonalConflictAt(i)) {
+          return true;
+        }
+      }
+
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
